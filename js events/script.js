662e44d7c794fdf5a7e0a411807880f1da027e5d
SomuTech <somuyadav018@gmail.com>
2021-10-17 20:49:45 +0530

    all

---
+++ js events/script.js
@@ -0,0 +1,36 @@
+//mouse events
+function function1() {
+    document.getElementById("fun1").innerHTML = "mouse move works";
+}
+
+function function2() {
+    document.getElementById("fun2").innerHTML = "mouse over works";
+}
+
+function function3() {
+    document.getElementById("fun3").innerHTML = "mouse leave works";
+}
+function function4() {
+    document.getElementById("fun4").style.color = "red";
+}
+
+
+//form events
+function form1() {
+    document.getElementById("fname").style.background = "red";
+}
+function form2() {
+    document.getElementById("lname").style.background = "gray";
+}
+function form3() {
+    document.getElementById("id").style.color = "green";
+}
+function form4() {
+    alert("Submitted Successfully");
+}
+
+
+//window events
+function wind1() {
+    alert("welcome")
+}
